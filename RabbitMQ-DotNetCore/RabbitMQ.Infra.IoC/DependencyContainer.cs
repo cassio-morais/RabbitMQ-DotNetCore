09ef20bfd5a846fa7a428c3215caf5e6feb5c02d
Cássio Morais <cassiomoraisg@hotmail.com>
2021-04-18 20:29:51 -0300

    add: Dependency Container

---
+++ RabbitMQ-DotNetCore/RabbitMQ.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RabbitMQ.Domain.Bus;
+using RabbitMQ.Infra.Bus;
+
+namespace RabbitMQ.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
